b2604f0a3cd97bb6c8ccf260cf8c209c84c2bd68
Steve Pilcher <stevepilcheris@gmail.com>
2020-05-27 09:54:14 +0000

    Layout created , JS started

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,27 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, roundCounter, dice1, dice2, dice3, dice4, dice5;
 
 scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+roundScore = 88;
+activePlayer = 1;
+roundCounter = 0;
 
-dice1 = Math.floor(Math.random() * 7);
-dice2 = Math.floor(Math.random() * 7);
-dice3 = Math.floor(Math.random() * 7);
-dice4 = Math.floor(Math.random() * 7);
-dice5 = Math.floor(Math.random() * 7);
+document.getElementById('score-box-' + activePlayer).textContent = roundScore;
+// document.querySelectorAll('.dice').style.display = 'none';
+/*
+dice1 = Math.floor(Math.random() * 6) + 1;
+dice2 = Math.floor(Math.random() * 6) + 1;
+dice3 = Math.floor(Math.random() * 6) + 1;
+dice4 = Math.floor(Math.random() * 6) + 1;
+dice5 = Math.floor(Math.random() * 6) + 1;
 
+document.getElementById('dice1').textContent = dice1;
+document.getElementById('dice2').textContent = dice2;
+document.getElementById('dice3').textContent = dice3;
+document.getElementById('dice4').textContent = dice4;
+document.getElementById('dice5').textContent = dice5;
 
-document.getElementById('diceA').textContent = dice1;
-document.getElementById('diceB').textContent = dice2;
-document.getElementById('diceC').textContent = dice3;
-document.getElementById('diceD').textContent = dice4;
-document.getElementById('diceE').textContent = dice5;
+*/
+
+if (dice1 === 1) {
+    diceInput = getElementById('dice1').innerHTML.src('')
+}
